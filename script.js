9ff94542276fcb684ef38a0f7aa1101c1982d46e
Dasha <girl.is.anime20@gmail.com>
2023-04-30 18:16:19 +0300

    init: start inheritance

---
+++ script.js
@@ -0,0 +1,21 @@
+class Builder {
+    constructor(value){
+        this.value = value
+
+    }
+    plus(...n)
+    minus(...n)
+    multiply(n)
+    divide(n)
+    get()
+}
+
+
+class IntBuilder extends Builder{
+
+
+}
+
+
+
+const build = new IntBuilder()
