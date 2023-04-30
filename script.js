03899ddd046464f7f0bd025bae7345d6387cf816
Dasha <girl.is.anime20@gmail.com>
2023-04-30 18:52:32 +0300

    feat: add methods in IntBuilder

--- script.js
+++ script.js
@@ -7,16 +7,38 @@ class Builder {
         for(let value of n){
             this.value += value
         }
+        return this
     }
-    minus(...n)
-    multiply(n)
-    divide(n)
-    get()
+    minus(...n){
+        for(let value of n){
+            this.value -= value
+        }
+        return this
+    }
+    multiply(n){
+        this.value *= value
+        return this
+    }
+    divide(n){
+        this.value /= value
+        this.value = parseInt(this.value)
+        return this
+    }
+    get(){
+        return this.value
+    }
 }
 
 
 class IntBuilder extends Builder{
+    mod(n){
+        this.value %= value
+        return this
+    }
 
+   static random(from, to){
+    return Math.round(Math.random() * (to - from) + from)
+    }
 
 }
 
